7686cc29f383d072ad8a026a3b4a46c8a1f542a8
Michal Guerquin <michalg@allenai.org>
2016-10-28 13:27:16 -0700

    remove document_id, add summary at the end

--- solvers/textsearch/src/universal/bin/insert-text-to-elasticsearch.py
+++ solvers/textsearch/src/universal/bin/insert-text-to-elasticsearch.py
@@ -12,39 +12,43 @@ import urllib
 ELASTIC_SEARCH_URL = 'http://localhost:9200/knowledge/sentence/_bulk'
 DOCUMENTS_PER_POST = 100000
 
-def payload_from_documents(post_documents):
+def make_payload_from_sentences(sentences):
   payload_lines = []
-  for (document_id, sentence) in post_documents:
-    payload_lines += [ json.dumps({"index":{"_id":str(document_id)}}) ]
+  for sentence in sentences:
+    payload_lines += [ json.dumps({"index":{}}) ]
     payload_lines += [ json.dumps({"body":sentence}) ]
   return "\n".join(payload_lines)
 
-def bulk_load_elasticsearch(post_documents):
-  payload = payload_from_documents(post_documents)
-  f = urllib.urlopen(ELASTIC_SEARCH_URL, payload)
-  response = json.loads(f.read())
+def bulk_load_elasticsearch(sentences):
+  payload = make_payload_from_sentences(sentences)
+  response_file = urllib.urlopen(ELASTIC_SEARCH_URL, payload)
+  response = json.loads(response_file.read())
   print "Posted %d documents (%d bytes) to %s. Elasticsearch errors = %s" % (
-    len(post_documents),
+    len(sentences),
     len(payload),
     ELASTIC_SEARCH_URL,
     str(response.get("errors", "?"))
   )
 
 def main():
-    document_id = 1
-    post_documents = []
+    line_count = 0
+    sentence_count = 0
+    sentences = []
     for line in sys.stdin:
+      line_count += 1
       line_cleaned = re.sub(r'([^a-zA-Z0-9\.])', " ", line).strip()
       for sentence in line_cleaned.split("."):
         if len(sentence) == 0:
           continue
-        post_documents += [(document_id, sentence)]
-        document_id += 1
-        if len(post_documents) % DOCUMENTS_PER_POST == 0:
-          bulk_load_elasticsearch(post_documents)
-          post_documents = []
+        sentence_count += 1
+        sentences += [sentence]
+        if len(sentences) % DOCUMENTS_PER_POST == 0:
+          bulk_load_elasticsearch(sentences)
+          sentences = []
     
-    bulk_load_elasticsearch(post_documents)
+    bulk_load_elasticsearch(sentences)
+    print "Lines read: ", line_count
+    print "Sentences posted: ", sentence_count
 
 if __name__ == "__main__":
     main()
